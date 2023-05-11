75846fd14c588d692c1264e410c77874f43cedf2
it-frilans <133250138+it-frilans@users.noreply.github.com>
2023-05-11 19:12:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$( "#btn1" ).on( "click", function() {
+    window.location.href = "contactme.html";
+});
+$( "#btn2" ).on( "click", function() {
+    window.location.href = "skills.html";
+});
+if  (screen.width <979) {
+    window.location.href = 'mobile/mobile.html';
+  }
